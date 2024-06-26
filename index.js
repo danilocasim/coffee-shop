02eb92a028628ce3a24fb4acd50f6329579eb89c
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-06-26 13:30:49 +0800

    Add side cart to main and animation

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// ANIMATION
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
 // NAVIGATION BAR
 const navSlide = () => {
   const burger = document.querySelector(".burger");
