c44966d18b5fd9510783381a5363e3fd86b5a66a
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-06-26 12:12:56 +0800

    Add the sidecart

--- index.js
+++ index.js
@@ -57,3 +57,29 @@ document.addEventListener("DOMContentLoaded", function () {
     drinksSection.style.display = "none";
   });
 });
+
+const openBtn = document.getElementById("open-cart-btn");
+const cart = document.getElementById("sidecart");
+const closeBtn = document.getElementById("close-btn");
+const backdrop = document.querySelector(".backdrop");
+
+openBtn.addEventListener("click", openCart);
+closeBtn.addEventListener("click", closeCart);
+backdrop.addEventListener("click", closeCart);
+
+// open cart
+function openCart() {
+  cart.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(() => {
+    backdrop.classList.add("show");
+  }, 0);
+}
+
+function closeCart() {
+  cart.classList.remove("open");
+  backdrop.classList.remove("show");
+  setTimeout(() => {
+    backdrop.style.display = none;
+  }, 500);
+}
